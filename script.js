7647b987fa7bd2215bc296d1c870debe06a48e64
surya pratap singh tomar <87597833+suryapratapsingh5674@users.noreply.github.com>
2024-02-10 21:47:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+var tl = gsap.timeline()
+
+tl.to("#yellow1", {
+    top:"-100%",
+    delay:0.5,
+    duration:0.5,
+    ease:"expo.out"
+})
+
+tl.to("#loader video",{
+    top:"-120%",
+    delay:0.5,
+    duration:0.5,
+    ease:"expo.out"
+}, "anim")
+
+tl.to("#loader h1", {
+    delay:0.2,
+    duration:0.5,
+    color:"black"
+}, "anim")
+
+tl.to("#loader", {
+    display:"none"
+})
+
+document.getElementById('arow').addEventListener('click', function(event) {
+    event.preventDefault(); 
+    const targetSection = document.getElementById('page2');
+    targetSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start' 
+    });
+});
+
+var elems = document.querySelectorAll(".elem")
+var page2 = document.querySelector("#page2")
+elems.forEach(function(ele){
+    ele.addEventListener("mouseenter", function(){
+        var bgimg = ele.getAttribute("data-img");
+        page2.style.backgroundImage = `url(${bgimg})`
+    })
+})
+
+document.getElementById('up').addEventListener('click', function(event) {
+    event.preventDefault(); 
+    const targetSection = document.getElementById('page1');
+    targetSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start' 
+    });
+});
